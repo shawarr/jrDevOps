fe34079940e043d049ade5c2cf69a8c1c9196e76
Ahmad Shawar <ahmadshawar@Ahmads-MacBook-Pro.local>
2026-01-14 22:08:53 +0300

    Added basic logging

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
+    app.logger.info("Home page requested")
     return ("Welcome to the application!", 200)
 
 @app.route('/health', methods=['GET'])
 def health():
+    app.logger.info("Health check requested")
     return ("OK", 200)
 
 @app.route('/info', methods=['GET'])
@@ -16,15 +18,18 @@ def info():
     import socket
     hostname= socket.gethostname()
     env = os.getenv('FLASK_ENV', 'development')
+    app.logger.info("Info requested: hostname=%s, env=%s", hostname, env)
     return jsonify({'version' : "1.0.0", 'hostname': hostname, 'env': env })
 
 @app.route('/visits', methods=['GET'])
 def visits():
+    app.logger.info("Visits page requested")
     from flask import render_template
     return render_template('visits.html')
 
 @app.route('/src/<path:filename>', methods=['GET'])
 def serve_static(filename):
+    app.logger.info("Static file requested: %s", filename)
     from flask import send_from_directory
     return send_from_directory('static', filename)
 
